6104eaeddab2d02b71c9c913c6ff095e99102f20
Jefferson Vivanco <jv1019@nyu.edu>
2016-09-24 19:07:16 -0400

    Second Commit

--- src/basketball.js
+++ src/basketball.js
@@ -1,3 +1,86 @@
 /**
  * Created by jeffersonvivanco on 9/21/16.
  */
+var gameData = require('../data.json');
+var stringV = gameData.arena;
+
+var gameId = "Game ID: "+gameData.id;
+
+var players = gameData.players; //All the players in the team
+
+//------------------------------------Getting total points for each team-------------------------//
+var pacersFinalScore  = 0;
+var hawksFinalScore = 0;
+function filterByTeam(player) {
+    if(player.team_name === "Hawks"){
+        var twoPointFGH = (parseInt(player.field_goals_made) - parseInt(player.three_pointers_made))*2;
+        hawksFinalScore = hawksFinalScore + parseInt(player.three_pointers_made)*3+parseInt(player.free_throws_made)+twoPointFGH;
+        return true;
+    }
+    else if(player.team_name === "Pacers"){
+        var twoPointFGP = (parseInt(player.field_goals_made) - parseInt(player.three_pointers_made))*2;
+        pacersFinalScore = pacersFinalScore + parseInt(player.three_pointers_made)*3+parseInt(player.free_throws_made)+twoPointFGP;
+        return true;
+    }
+    else
+        return false;
+}
+var teamFilter = players.filter(filterByTeam);
+console.log("Pacers: "+pacersFinalScore+"\nHawks: "+hawksFinalScore+"\n");//Printing final score of Pacers and Hawks
+//------------------------------------------------------------------------------------------------//
+
+//------------------------------------Player with the most rebounds-------------------------//
+
+function getMostRebounds(prev, curr){
+    var totalReboundsPrev = parseInt(prev.rebounds_offensive)+parseInt(prev.rebounds_defensive);
+    var totalReboundsCurr = parseInt(curr.rebounds_offensive)+parseInt(curr.rebounds_defensive);
+    var maxRebounds = Math.max(totalReboundsCurr, totalReboundsPrev);
+    if(maxRebounds === totalReboundsPrev){
+        return prev;
+    }
+    else{
+        return curr;
+    }
+
+}
+var teamReduce  = players.reduce(getMostRebounds);
+var totalRebounds = parseInt(teamReduce.rebounds_offensive)+parseInt(teamReduce.rebounds_defensive);
+console.log("* Most rebounds: "+teamReduce.first_name+" "+teamReduce.last_name+" with "+totalRebounds);
+//------------------------------------------------------------------------------------------------//
+
+//------------------------------------Guard(G) with highest three pointer percentage-------------------------//
+function filterIfGuard(player) {
+    if(player.position_short === 'G'){
+        return true;
+    }
+    else
+        return false;
+}
+function getGuardHighestThreePer(prev, curr) {
+    var threePerPrev = parseInt(prev.three_pointers_made)/parseInt(prev.three_pointers_attempted);
+    var threePerCurr = parseInt(curr.three_pointers_made)/parseInt(curr.three_pointers_attempted);
+    var highestPer = Math.max(threePerCurr,threePerPrev);
+    if(highestPer === threePerPrev)
+        return prev;
+    else
+        return curr;
+}
+var guardFilter = players.filter(filterIfGuard);
+var highestThreePer = guardFilter.reduce(getGuardHighestThreePer);
+var threePointerGuardName = highestThreePer.first_name+" "+highestThreePer.last_name;
+var threePointerGuardPercent = (highestThreePer.three_pointers_made/highestThreePer.three_pointers_attempted)*100;
+console.log("* Guard (G) with highest 3 point percentage: "+threePointerGuardName+" at "+ threePointerGuardPercent+"%.");
+//------------------------------------------------------------------------------------------------//
+
+//------------------------------------Total number of players with at least one assist-------------------------//
+var numOfPlayersWithAtleastOneAssist = 0;
+function getPlayersWithAtleastOneAssist(player) {
+    if(player.assists > 0)
+        numOfPlayersWithAtleastOneAssist++;
+}
+var playersWithAtLeastOneAssist = players.forEach(getPlayersWithAtleastOneAssist);
+console.log("There were "+numOfPlayersWithAtleastOneAssist +" players that had at least one assist")
+//------------------------------------------------------------------------------------------------//
+
+
+// console.log(players);
--- src/exercises.js
+++ src/exercises.js
@@ -10,8 +10,49 @@ var functions = {
         }
         return total;
     },
-    repeatCall : function () {
-        
+    repeatCall : function (fn, n, arg) {
+        for(var i=0; i<n; i++){
+            fn(arguments[2]);
+        }
+    },
+    repeatCallAllArgs : function (fn, n, args) {
+        var array = Array.prototype.slice.call(arguments, 2);
+        for(var i=0; i<n; i++){
+            fn.apply(this,array);
+        }
+    },
+    makePropertyChecker : function (prop) {
+        return function(obj){
+            return obj.hasOwnProperty(prop);
+        }
+    },
+    constrainDecorator : function (fn, min, max) {
+        return function(){
+            if(fn.apply(this, arguments) < min){
+                return min;
+            }
+            else if(fn.apply(this, arguments)>max){
+                return max;
+            }
+            else{
+                return fn.apply(this, arguments);
+            }
+        }
+
+    },
+    limitCallsDecorator : function(fn, n){
+        var numOfTimesCalled = 0;
+        return function(){
+            if(numOfTimesCalled < n){
+                numOfTimesCalled++;
+                return fn.apply(this, arguments);
+            }
+            else
+                return undefined;
+        }
     }
-}
+};
+
+
+
 module.exports = functions;
